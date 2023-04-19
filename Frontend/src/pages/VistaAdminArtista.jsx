bbe44145af363369fd932211e21be70563ec6165
Daniel <93655483+Danielrg37@users.noreply.github.com>
2023-04-19 11:05:28 +0200

    Update VistaAdminArtista.jsx

--- Frontend/src/pages/VistaAdminArtista.jsx
+++ Frontend/src/pages/VistaAdminArtista.jsx
@@ -1,31 +1,4 @@
-
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/vista_artista.css';
-import { useNavigate } from 'react-router-dom';
-
-function VistaArtista() {
-
-    return (
-
-        <div className="container">
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    Placeholder
-                </a>
-
-                <ul className="nav nav-pills">
-                    <Button className="green-color" onClick={() => navigate('/registro')}>
-                        Placeholder
-                    </Button>
-                </ul>
-            </header>
-
-            <div className="row">
-                import React, { useState } from 'react';
+import React, { useState } from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
@@ -89,31 +62,3 @@ export default function SearchBar() {
     </div>
   );
 }
-
-            </div>
-
-            <div class="row mt-3">
-
-
-            <div class="row mt-3">
-                
-            </div>
-
-            <div className="row mt-3">
-                
-            </div>
-
-            <div class="row mt-5">
-                
-            </div>
-
-            <footer>
-                <p class="float-end"><a href="#">Back to top</a></p>
-                <p>Placeholder <a href="#">Placeholder</a> · <a href="#"></a></p>
-
-            </footer>
-        </div>
-    );
-}
-
-export default VistaArtista;
