d9c1e63c295034d7460264ebcf6886add586964c
Daniel <93655483+Danielrg37@users.noreply.github.com>
2023-04-19 11:02:44 +0200

    Añadido searchBar con Api de Spotify
    Añadido cambios en searchBar

--- Frontend/src/pages/VistaAdminArtista.jsx
+++ Frontend/src/pages/VistaAdminArtista.jsx
@@ -25,7 +25,70 @@ function VistaArtista() {
             </header>
 
             <div className="row">
+                import React, { useState } from 'react';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  inputRoot: {
+    backgroundColor: 'white',
+    '& .MuiOutlinedInput-notchedOutline': {
+      borderColor: 'gray',
+    },
+    '&:hover .MuiOutlinedInput-notchedOutline': {
+      borderColor: 'gray',
+    },
+    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#1DB954',
+    },
+  },
+  option: {
+    backgroundColor: 'white',
+    color: 'black',
+  },
+  getLabel: {
+    padding: theme.spacing(1),
+    backgroundColor: '#1DB954',
+    color: 'white',
+    fontWeight: 'bold',
+    borderTopRightRadius: 4,
+    borderBottomRightRadius: 4,
+  },
+}));
+
+export default function SearchBar() {
+  const classes = useStyles();
+  const [value, setValue] = useState(null);
+
+  return (
+    <div style={{ display: 'flex', maxWidth: 400 }}>
+      <div className={classes.getLabel}>GET</div>
+      <Autocomplete
+        id="search-bar"
+        options={[]}
+        getOptionLabel={(option) => option}
+        style={{ width: '100%' }}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Search for an artist..."
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+              classes: { root: classes.inputRoot },
+            }}
+          />
+        )}
+        classes={{ option: classes.option }}
+      />
+    </div>
+  );
+}
 
             </div>
 
@@ -46,7 +109,7 @@ function VistaArtista() {
 
             <footer>
                 <p class="float-end"><a href="#">Back to top</a></p>
-                <p>Placeholder <a href="#">Placeholder</a> · <a href="#"></a></p>
+                <p>Placeholder <a href="#">Placeholder</a> Â· <a href="#"></a></p>
 
             </footer>
         </div>
