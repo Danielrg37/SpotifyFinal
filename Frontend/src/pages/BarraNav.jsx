a7f2c9faa67ff2e1b7bda5e266fa24946a528727
Danielrg37 <danielrg37@educastur.es>
2023-05-24 11:02:47 +0000

    Añadida preparación para menú final

--- Frontend/src/pages/BarraNav.jsx
+++ Frontend/src/pages/BarraNav.jsx
@@ -1,80 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/home/home.css';
 import fernando from './../img/fernando.png';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import imagenBack from './../img/foto2.png';
 
+function BarraNav() {
+  const navigate = useNavigate();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-function BarraNav(){
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-const navigate = useNavigate();
+  const handleDropdownClose = () => {
+    setIsDropdownOpen(false);
+  };
 
-
-
-
-
-// return (
-//     <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-//     <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none" style={{ marginRight: 'auto' }}>
-//       Placeholder
-//     </a>
-//     <ul className="nav nav-pills" style={{ marginLeft: 'auto' }}>
-//       <Button className="green-color" onClick={() => navigate('login')}>
-//         Placeholder
-//       </Button>
-//     </ul>
-//     <style>
-//       {`@media (max-width: 767px) {
-//         .nav-pills {
-//           margin-left: 10px; /* Ajusta el valor según tus necesidades */
-//         }
-//       }`}
-//     </style>
-//   </header>
-// )
-// } else 
-
-if(localStorage.getItem('token') != null) {
-return (
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+  return (
+    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         Placeholder
-    </a>
-    <ul class="nav nav-pills">
+      </a>
+      <ul className="nav nav-pills">
         <Button className="green-color" onClick={() => navigate('login2')}>
-            Placeholder
+          Placeholder
         </Button>
 
         <Button className="green-color" onClick={() => navigate('perfil')}>
-            Perfil rápido
+          Perfil rápido
         </Button>
 
- 
         <Button className="green-color" onClick={() => navigate('admin')}>
-            Admin rápido
+          Admin rápido
         </Button>
-    </ul>
 
-</header>
-)
-} else {
-return (
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        Placeholder
-    </a>
-    <ul class="nav nav-pills">
-        <Button className="green-color" onClick={() => navigate('login2')}>
-            Placeholder
-        </Button>
-    </ul>
+        <div className="dropdown" onMouseLeave={handleDropdownClose}>
+          <img
+            src="your-image.jpg"
+            alt="Your Image"
+            className="dropdown-img"
+            onMouseEnter={handleDropdownToggle}
+          />
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              <a href="#">Option 1</a>
+              <a href="#">Option 2</a>
+              <a href="#">Option 3</a>
+            </div>
+          )}
+        </div>
+      </ul>
     </header>
-)
-}
+  );
 }
 
 export default BarraNav;
