c53c5244197812f6fb5fb95921b267ed57452694
Daniel <93655483+Danielrg37@users.noreply.github.com>
2023-06-17 17:20:13 +0200

    Update VistaAdminUsuario.jsx

--- Frontend/src/pages/VistaAdminUsuario.jsx
+++ Frontend/src/pages/VistaAdminUsuario.jsx
@@ -42,19 +42,22 @@ function VistaAdminUsuario() {
       });
   }, []);
 
-useEffect(() => {
-  fetch('http://ec2-3-230-86-196.compute-1.amazonaws.com:5120/acciones/acciones?limit=10', {
-    method: 'GET',
-    headers: {
-      Origin: 'http://localhost:5173',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      setAcciones(data);
-    });
-}, []);
+  
+  useEffect(() => {
+    fetch('http://ec2-3-230-86-196.compute-1.amazonaws.com:5120/acciones/acciones?limit=10', {
+      method: 'GET',
+      headers: {
+        Origin: 'http://localhost:5173',
+      },
+    })
+      .then((res) => res.json())
+     
+        .then((data) => {
+          console.log(data);
+          const firstTenRecords = data.slice(0, 10);
+          setAcciones(firstTenRecords);
+        });
+  }, []);
 
 
   const borrar = (id) => {
