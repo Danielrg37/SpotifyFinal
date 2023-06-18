343e4b10cdf328c801b37d40a4ac465bbbc242b8
Daniel <93655483+Danielrg37@users.noreply.github.com>
2023-06-18 12:38:19 +0200

    Update VistaAdminCancion.jsx

--- Frontend/src/pages/VistaAdminCancion.jsx
+++ Frontend/src/pages/VistaAdminCancion.jsx
@@ -89,7 +89,7 @@ function VistaAdminArtista() {
     return <Error404 />;
   }
   
-  if(localStorage.getItem('nombreUsuario') == 'admin'){
+  if(localStorage.getItem('nombreUsuario') != 'admin'){
     return <Error404 />;
   }
   
